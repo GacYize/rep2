3a70f76d02810b6d34c6766c28547b2e2d58ff3c
admin <admin@163.com>
2018-12-10 20:55:35 +0800

    添加账户实体类

---
+++ day01_mybatis02/src/main/java/com/ali/domain/Account.java
@@ -0,0 +1,22 @@
+package com.ali.domain;
+
+public class Account {
+    private String accountId;
+    private Integer money;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+}
