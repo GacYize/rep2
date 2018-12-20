cc146b1c434bf318f76285822358ed743e0929a8
admin <admin@163.com>
2018-12-20 13:48:00 +0800

    添加test实体类添加属性

--- day01_mybatis02/src/main/java/com/ali/domain/Test.java
+++ day01_mybatis02/src/main/java/com/ali/domain/Test.java
@@ -2,6 +2,15 @@ package com.ali.domain;
 
 public class Test {
     private String name;
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
